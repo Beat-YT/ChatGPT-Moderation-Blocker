3d19b775a64cc76348138ac8716d3ecbccc7dbab
Beat YT <66485277+Beat-YT@users.noreply.github.com>
2023-05-18 12:04:20 -0400

    update for chrome web store

--- src/modapi-disabler.js
+++ src/modapi-disabler.js
@@ -1,31 +1,32 @@
-// This script is injected into the page to disable the modapi
+(function () {
+    // This script is injected into the page to disable the modapi
 
-// first we make a backup of the original fetch function
-const _fetch = window.fetch;
+    // first we make a backup of the original fetch function
+    const _fetch = window.fetch;
 
-// then we replace it with a function that will modify 
-// the request parameters to set the modapi to false
+    // then we replace it with a function that will modify 
+    // the request parameters to set the modapi to false
 
-window.fetch = function (url, options) {
-
-    // we only want to modify the request to the conversation endpoint and only if the modapi is enabled
-    if (url.startsWith("https://chat.openai.com/backend-api/conversation")
-        && options.method === "POST" &&
-        options.body?.includes("supports_modapi")
-    ) {
-        // we parse the body and set the modapi to false 
+    window.fetch = function (url, options) {
+        // we only want to modify the request to the conversation endpoint and only if the modapi is enabled
         try {
-            const body = JSON.parse(options.body);
 
-            if (body['supports_modapi'] != undefined) {
-                console.log("[ModAPI Disabler - ChatGPT Moderation Blocker] Patched ModAPI");
+            if (url && options &&
+                url.startsWith("https://chat.openai.com/backend-api/conversation") &&
+                options.method === "POST"
+            ) {
+                // we parse the body and set the modapi support to false
+                const body = JSON.parse(options.body);
                 body['supports_modapi'] = false;
                 options.body = JSON.stringify(body);
+                console.log("[ModAPI Disabler - ChatGPT Moderation Blocker] Patched ModAPI")
             }
+
         } catch (e) {
-            console.error(e);
+            console.error(e)
         }
+
+        return _fetch.apply(this, arguments);
     }
 
-    return _fetch(url, options);
-}
+})();
